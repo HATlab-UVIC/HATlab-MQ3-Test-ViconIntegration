4f23ebf8795cdb400eea57aae68a0390f645bd5c
David Kim <46164193+daehwankim112@users.noreply.github.com>
2024-06-12 12:01:59 -0700

    chore: ! debugging analog input from Vicon

--- Assets/Scripts/Analog_Input.cs
+++ Assets/Scripts/Analog_Input.cs
@@ -11,20 +11,21 @@ public class Analog_Input : MonoBehaviour
     [SerializeField] string ComponentName;
     [SerializeField] TextMeshProUGUI AnalogOutputText;
     [SerializeField] Light SpotLight;
-    
+    bool IsReadingInput = false;
 
-    void Update()
+    void Start()
     {
-        if (Client == null) Debug.LogError("ViconDataStreamClient does not exist for analog input");
+        if (Client == null) DebugConsole.Error("ViconDataStreamClient does not exist for analog input");
         if (Client.IsDeviceDataEnabled().Enabled)
         {
             if (Client.GetDeviceCount().DeviceCount > 0)
-            {
-                Debug.LogError($"Analog device count: {Client.GetDeviceCount().DeviceCount}");
+            {   
+                DebugConsole.Log($"Analog device count: {Client.GetDeviceCount().DeviceCount}");
                 if (Client.GetDeviceOutputValue(DeviceName, ComponentName).Result == Result.Success)
                 {
                     AnalogOutputText.text = Client.GetDeviceOutputValue(DeviceName, ComponentName).Value.ToString("0.00");
                     SpotLight.intensity = (float) Client.GetDeviceOutputValue(DeviceName, ComponentName).Value;
+                    IsReadingInput = true;
                 }
             }
             else
@@ -39,5 +40,66 @@ public class Analog_Input : MonoBehaviour
             Debug.LogError("Client Device Data is not enabled");
             Client.EnableDeviceData();
         }
+
+        StartCoroutine(check());
+    }
+
+    void Update()
+    {
+        if (IsReadingInput && Client.IsDeviceDataEnabled().Enabled)
+        {
+            if (Client.GetDeviceOutputValue(DeviceName, ComponentName).Result == Result.Success)
+            {
+                AnalogOutputText.text = Client.GetDeviceOutputValue(DeviceName, ComponentName).Value.ToString("0.00");
+                SpotLight.intensity = (float)Client.GetDeviceOutputValue(DeviceName, ComponentName).Value;
+                IsReadingInput = true;
+            }
+        }
+    }
+
+    IEnumerator check()
+    {
+        while (true)
+        {
+            if (Client.IsDeviceDataEnabled().Enabled)
+            {
+                DebugConsole.Log("Device Data is enabled");
+                if (Client.GetDeviceCount().DeviceCount > 0)
+                {
+                    DebugConsole.Log($"Device count is: {Client.GetDeviceCount().DeviceCount}");
+                    if (Client.GetDeviceOutputValue(DeviceName, ComponentName).Result == Result.Success)
+                    {
+                        DebugConsole.Success("Client successfully reading device output value");
+                        IsReadingInput = true;
+                    }
+                    else
+                    {
+                        DebugConsole.Error($"Client unsuccessfully reading device output value.\nGetDeviceOutputValue Result: {Client.GetDeviceOutputValue(DeviceName, ComponentName).Result.ToString()}");
+                        IsReadingInput = false;
+                    }
+                }
+                else
+                {
+                    DebugConsole.Log($"Device count is: {Client.GetDeviceCount().DeviceCount}");
+                    IsReadingInput= false;
+                }
+            }
+            else
+            {
+                IsReadingInput = false;
+                DebugConsole.Error("Device Data is not enabled");
+                DebugConsole.Log("Trying enabling Deice Data...");
+                Client.EnableDeviceData();
+                if(Client.IsDeviceDataEnabled().Enabled)
+                {
+                    DebugConsole.Success("Device Data is enabled");
+                }
+                else
+                {
+                    DebugConsole.Error("Device Data is cannot be enabled");
+                }
+            }
+        }
+        yield return new WaitForSeconds(1f);
     }
 }
